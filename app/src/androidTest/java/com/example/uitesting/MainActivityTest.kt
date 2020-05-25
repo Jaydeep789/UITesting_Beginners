9e70fda919ee7d1fd17c4f0c7bd4a150840530b6
Jaydeep789 <shetyejaydeep@gmail.com>
2020-05-25 13:23:19 -0400

    Basic Activity Navigation Tests

--- app/src/androidTest/java/com/example/uitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/uitesting/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.example.uitesting
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -34,5 +35,24 @@ class MainActivityTest {
     fun test_isButton_inView() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_navToSecondaryActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(click())
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 }
